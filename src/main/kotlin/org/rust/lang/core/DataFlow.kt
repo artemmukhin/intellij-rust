c3eb29b8a44f5580ed1c12161e7587ee8b7185b1
ortem <ortem00@gmail.com>
2018-07-17 14:46:36 +0300

    Add applyGenKill, eachBit, bitwise

--- src/main/kotlin/org/rust/lang/core/DataFlow.kt
+++ src/main/kotlin/org/rust/lang/core/DataFlow.kt
@@ -7,6 +7,7 @@ package org.rust.lang.core
 
 import org.rust.lang.core.psi.RsBlock
 import org.rust.lang.core.psi.ext.RsElement
+import java.util.*
 
 enum class EntryOrExit { Entry, Exit }
 
@@ -17,9 +18,9 @@ class DataFlowContext<O : DataFlowOperator>(val analysisName: String,
                                             val bitsPerId: Int) {
     val bitsPerInt: Int = 32
     val wordsPerId: Int
-    val gens: MutableList<Int>
-    val scopeKills: MutableList<Int>
-    val actionKills: MutableList<Int>
+    val gens: MutableList<Int>          //
+    val scopeKills: MutableList<Int>    // todo: maybe it's better to use BitSet
+    val actionKills: MutableList<Int>   //
     val onEntry: MutableList<Int>
     val localIndexTable: HashMap<RsElement, MutableList<NodeIndex>>
 
@@ -36,6 +37,8 @@ class DataFlowContext<O : DataFlowOperator>(val analysisName: String,
     }
 
     private fun getCfgIndices(element: RsElement) = localIndexTable.getOrDefault(element, mutableListOf())
+
+    private fun hasBitsetForLocalElement(element: RsElement): Boolean = localIndexTable.containsKey(element)
 
     private fun computeIdRange(idx: NodeIndex): Pair<Int, Int> {
         val start = idx.index * wordsPerId
@@ -70,11 +73,77 @@ class DataFlowContext<O : DataFlowOperator>(val analysisName: String,
         }
     }
 
+    fun applyGenKill(idx: NodeIndex, bits: List<Int>): MutableList<Int> {
+        val (start, end) = computeIdRange(idx)
+        val result = bits.toMutableList()
+        Union.bitwise(result, gens.subList(start, end))
+        Subtract.bitwise(result, actionKills.subList(start, end))
+        Subtract.bitwise(result, scopeKills.subList(start, end))
+        return result
+    }
 
+    fun eachBitOnEntry(element: RsElement, f: (Int) -> Boolean): Boolean {
+        if (!hasBitsetForLocalElement(element)) return true
+        val indices = getCfgIndices(element)
+        return indices.all { eachBitForNode(EntryOrExit.Entry, it, f) }
+    }
+
+    fun eachBitForNode(e: EntryOrExit, idx: NodeIndex, f: (Int) -> Boolean): Boolean {
+        if (bitsPerId == 0) return true
+
+        val (start, end) = computeIdRange(idx)
+        val onEntry = onEntry.subList(start, end)
+        val slice = when (e) {
+            EntryOrExit.Entry -> onEntry
+            EntryOrExit.Exit -> applyGenKill(idx, onEntry)
+        }
+        return eachBit(slice, f)
+    }
+
+    fun eachBit(words: List<Int>, f: (Int) -> Boolean): Boolean {
+        words.filter { it != 0 }.forEachIndexed { index, word ->
+            val baseIndex = index * bitsPerInt
+            for (offset in 0..bitsPerInt) {
+                val bit = 1 shl offset
+                if (word and bit != 0) {
+                    val bitIndex = baseIndex + offset
+                    if (bitIndex >= bitsPerId) {
+                        return true
+                    } else if (!f(index)) {
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
+
+    // todo
+    fun addKillsFromFlowExits() {}
 }
 
 interface BitwiseOperator {
     fun join(succ: Int, pred: Int): Int
+
+    fun bitwise(outBits: MutableList<Int>, inBits: List<Int>): Boolean {
+        var changed = false
+
+        outBits.zip(inBits).forEachIndexed { i, (outBit, inBit) ->
+            val newValue = join(outBit, inBit)
+            outBits[i] = newValue
+            changed = changed or (outBit != newValue)
+        }
+
+        return changed
+    }
+}
+
+object Union : BitwiseOperator {
+    override fun join(succ: Int, pred: Int) = succ or pred
+}
+
+object Subtract : BitwiseOperator {
+    override fun join(succ: Int, pred: Int) = succ and pred.inv()
 }
 
 interface DataFlowOperator : BitwiseOperator {
