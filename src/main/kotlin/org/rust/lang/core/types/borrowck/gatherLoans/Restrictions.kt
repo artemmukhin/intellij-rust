8144e17b8438327e4ec11333a48244e6453adbc3
ortem <ortem00@gmail.com>
2018-08-03 15:37:37 +0300

    Refactor Restrictions

--- src/main/kotlin/org/rust/lang/core/types/borrowck/gatherLoans/Restrictions.kt
+++ src/main/kotlin/org/rust/lang/core/types/borrowck/gatherLoans/Restrictions.kt
@@ -10,11 +10,14 @@ import org.rust.lang.core.psi.ext.RsStructKind
 import org.rust.lang.core.psi.ext.kind
 import org.rust.lang.core.psi.ext.namedFields
 import org.rust.lang.core.types.borrowck.*
+import org.rust.lang.core.types.borrowck.BorrowCheckErrorCode.BorrowedPointerTooShort
+import org.rust.lang.core.types.borrowck.LoanPathElement.Interior
 import org.rust.lang.core.types.borrowck.LoanPathKind.*
 import org.rust.lang.core.types.borrowck.gatherLoans.AliasableViolationKind.BorrowViolation
 import org.rust.lang.core.types.borrowck.gatherLoans.RestrictionResult.Safe
 import org.rust.lang.core.types.borrowck.gatherLoans.RestrictionResult.SafeIf
 import org.rust.lang.core.types.infer.*
+import org.rust.lang.core.types.infer.InteriorKind.InteriorField
 import org.rust.lang.core.types.regions.Region
 import org.rust.lang.core.types.ty.TyAdt
 import org.rust.lang.core.types.ty.TyUnknown
@@ -36,26 +39,33 @@ class RestrictionContext(val bccx: BorrowCheckContext, val loanRegion: Region, v
         val category = cmt.category
         return when (category) {
             is Categorization.Rvalue -> Safe
+
             is Categorization.StaticItem -> Safe
+
             is Categorization.Upvar -> SafeIf(loanPath(Upvar()), mutableListOf(loanPath(Upvar())))
+
             is Categorization.Local -> SafeIf(loanPath(Var(category.element)), mutableListOf(loanPath(Var(category.element))))
+
             is Categorization.Deref -> {
+                val baseCmt = category.cmt
                 val pointerKind = category.pointerKind
                 when (pointerKind) {
                     is PointerKind.BorrowedPointer -> {
-                        if (bccx.isSubregionOf(loanRegion, pointerKind.region)) {
-                            bccx.report(BorrowCheckError(BorrowViolation(cause), category.cmt, BorrowCheckErrorCode.BorrowedPointerTooShort(loanRegion, pointerKind.region)))
+                        if (!bccx.isSubregionOf(loanRegion, pointerKind.region)) {
+                            val errorCode = BorrowedPointerTooShort(loanRegion, pointerKind.region)
+                            bccx.report(BorrowCheckError(BorrowViolation(cause), baseCmt, errorCode))
                             return Safe
                         }
 
                         when (pointerKind.borrowKind) {
                             BorrowKind.ImmutableBorrow -> Safe
-                            BorrowKind.MutableBorrow -> extend(restrict(category.cmt), cmt, LoanPathElement.Deref(pointerKind))
+                            BorrowKind.MutableBorrow -> extend(restrict(baseCmt), cmt, LoanPathElement.Deref(pointerKind))
                         }
                     }
                     is PointerKind.UnsafePointer -> TODO()
                 }
             }
+
             is Categorization.Interior -> {
                 val baseCmt = category.cmt
                 val baseType = baseCmt.ty
@@ -66,22 +76,23 @@ class RestrictionContext(val bccx: BorrowCheckContext, val loanRegion: Region, v
                         is Safe -> Safe
                         is SafeIf -> {
                             baseType.item.namedFields.forEachIndexed { i, field ->
-                                val fieldInteriorKind = InteriorKind.InteriorField(FieldIndex(i, field.name))
+                                val fieldInteriorKind = InteriorField(FieldIndex(i, field.name))
                                 val fieldType = if (fieldInteriorKind == category.interiorKind) cmt.ty else TyUnknown
-                                val siblingLpKind = Extend(result.loanPath, cmt.mutabilityCategory, LoanPathElement.Interior(variant, fieldInteriorKind))
+                                val siblingLpKind = Extend(result.loanPath, cmt.mutabilityCategory, Interior(variant, fieldInteriorKind))
                                 val siblingLp = LoanPath(siblingLpKind, fieldType)
                                 result.loanPaths.add(siblingLp)
                             }
-                            val lp = loanPath(Extend(result.loanPath, cmt.mutabilityCategory, LoanPathElement.Interior(variant, category.interiorKind)))
+                            val lp = loanPath(Extend(result.loanPath, cmt.mutabilityCategory, Interior(variant, category.interiorKind)))
                             SafeIf(lp, result.loanPaths)
                         }
                     }
                 } else {
-                    return extend(result, cmt, LoanPathElement.Interior(variant, category.interiorKind))
+                    return extend(result, cmt, Interior(variant, category.interiorKind))
                 }
             }
 
             is Categorization.Downcast -> restrict(category.cmt)
+
             null -> Safe
         }
     }
