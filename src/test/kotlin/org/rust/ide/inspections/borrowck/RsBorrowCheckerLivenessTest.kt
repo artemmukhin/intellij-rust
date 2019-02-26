6a86ca252572c2c214d9cd48971eede60f0bd612
ortem <ortem00@gmail.com>
2019-02-26 15:50:23 +0300

    temp

--- src/test/kotlin/org/rust/ide/inspections/borrowck/RsBorrowCheckerLivenessTest.kt
+++ src/test/kotlin/org/rust/ide/inspections/borrowck/RsBorrowCheckerLivenessTest.kt
@@ -5,16 +5,32 @@
 
 package org.rust.ide.inspections.borrowck
 
+import org.rust.ProjectDescriptor
+import org.rust.WithStdlibRustProjectDescriptor
 import org.rust.ide.inspections.RsBorrowCheckerInspection
 import org.rust.ide.inspections.RsInspectionsTestBase
 
 class RsBorrowCheckerLivenessTest : RsInspectionsTestBase(RsBorrowCheckerInspection()) {
 
-    fun `test 123`() = checkByText("""
-        fn foo(x: i32) {}
+    fun `test unused arguments empty body`() = checkByText("""
+        fn foo(<warning descr="Unused parameter">x</warning>: i32, <warning descr="Unused parameter">y</warning>: String) {}
     """, checkWarn = true)
 
-    fun `test move by call`() = checkByText("""
+    fun `test simple used argument`() = checkByText("""
+        fn foo(x: i32) -> i32 {
+            return x + 5;
+        }
+    """)
+
+    fun `test simple used variable`() = checkByText("""
+        fn foo() -> i32 {
+            let x = 10;
+            return x + 5;
+        }
+    """)
+
+    @ProjectDescriptor(WithStdlibRustProjectDescriptor::class)
+    fun `test use extending`() = checkByText("""
         #[derive(Copy, Clone)]
         struct S { data: i32 }
 
@@ -22,12 +38,9 @@ class RsBorrowCheckerLivenessTest : RsInspectionsTestBase(RsBorrowCheckerInspect
             let x = S { data: 42 };
             let mut i = 0;
             while i < 10 {
-                if x.data == 10 { f(x); } else {}
                 i += 1;
             }
             x;
         }
-
-        fn f(s: S) {}
-    """, checkWarn = true)
+ """, checkWarn = true)
 }
